a0ca5e1b1a4c2e837e65c03af3abbed212d7f736
Jan Vincent Liwanag <jvliwanag@gmail.com>
2019-10-30 14:27:18 +0800

    Format with purty

--- src/ORecord.purs
+++ src/ORecord.purs
@@ -1,29 +1,28 @@
 module ORecord
-       ( ORecord
-       , orecord
-       , class ORecordMapping
-       , toORecord
-       , fromORecord
-       , class MapMaybe
-       , class MapMaybeRL
-       , allRequired
-       , allOptional
-       -- Accessors
-       , getRequired
-       , getOptional
-       , setRequired
-       , setOptional
-       -- Utils
-       , unrequire
-       -- Helpers
-       , class RowKeys
-       , _rowKeys
-       , class RowKeysRL
-       , _rowKeysRL
-       ) where
+  ( ORecord
+  , orecord
+  , class ORecordMapping
+  , toORecord
+  , fromORecord
+  , class MapMaybe
+  , class MapMaybeRL
+  , allRequired
+  , allOptional
+  -- Accessors
+  , getRequired
+  , getOptional
+  , setRequired
+  , setOptional
+  -- Utils
+  , unrequire
+  -- Helpers
+  , class RowKeys
+  , _rowKeys
+  , class RowKeysRL
+  , _rowKeysRL
+  ) where
 
 import Prelude
-
 import Data.List (List)
 import Data.List as List
 import Data.Maybe (Maybe(..), fromMaybe)
@@ -36,78 +35,76 @@ import Type.Data.RowList (RLProxy(..))
 import Type.Prelude (class ListToRow)
 import Unsafe.Coerce (unsafeCoerce)
 
-foreign import data ORecord :: # Type -> # Type -> Type
+foreign import data ORecord :: #Type -> #Type -> Type
 
-orecord :: forall g required optional o o'. Union required o g => Union o o' optional => Nub g g => { |g } -> ORecord required optional
+orecord :: forall g required optional o o'. Union required o g => Union o o' optional => Nub g g => { | g } -> ORecord required optional
 orecord = unsafeCoerce
 
 instance orecordEq ::
   ( ORecordMapping required optional r
-  , Eq {|r}
-  ) => Eq (ORecord required optional) where
-  eq a a' = (fromORecord a :: {|r}) == fromORecord a'
+  , Eq { | r }
+  ) =>
+  Eq (ORecord required optional) where
+  eq a a' = (fromORecord a :: { | r }) == fromORecord a'
 
 instance orecordOrd ::
   ( ORecordMapping required optional r
-  , Ord {|r}
+  , Ord { | r }
   , Eq (ORecord required optional)
-  ) => Ord (ORecord required optional) where
-  compare a a' = compare (fromORecord a :: {|r}) (fromORecord a')
+  ) =>
+  Ord (ORecord required optional) where
+  compare a a' = compare (fromORecord a :: { | r }) (fromORecord a')
 
 instance orecordShow ::
   ( ORecordMapping required optional r
-  , Show {|r}
-  ) => Show (ORecord required optional) where
-  show a = "(ORecord " <> show (fromORecord a :: {|r}) <> ")"
+  , Show { | r }
+  ) =>
+  Show (ORecord required optional) where
+  show a = "(ORecord " <> show (fromORecord a :: { | r }) <> ")"
 
-class ORecordMapping
-      (required :: # Type)
-      (optional :: # Type)
-      (r :: # Type)
-      | r -> required optional, required optional -> r where
-  toORecord :: { |r } -> ORecord required optional
-  fromORecord :: ORecord required optional -> { |r }
+class ORecordMapping (required :: #Type) (optional :: #Type) (r :: #Type) | r -> required optional, required optional -> r where
+  toORecord :: { | r } -> ORecord required optional
+  fromORecord :: ORecord required optional -> { | r }
 
 instance orecordMappingInstance ::
   ( RowKeys optional
   , MapMaybe optional maybes
   , Union required maybes r
-  ) => ORecordMapping required optional r where
+  ) =>
+  ORecordMapping required optional r where
   toORecord = toORecordImpl (fromMaybe undefined) (rowKeys (RProxy :: _ optional))
   fromORecord = fromORecordImpl Nothing Just (rowKeys (RProxy :: _ optional))
 
-foreign import toORecordImpl :: forall a a' r required optional. (Maybe a -> a') -> Array String -> { |r } -> ORecord required optional
+foreign import toORecordImpl :: forall a a' r required optional. (Maybe a -> a') -> Array String -> { | r } -> ORecord required optional
 
-class MapMaybe (inr :: # Type) (outr :: # Type) | inr -> outr
+class MapMaybe (inr :: #Type) (outr :: #Type) | inr -> outr
 
 instance mapMaybeInstance ::
   ( RowToList inr inRl
   , MapMaybeRL inRl outRl
   , ListToRow outRl outr
-  ) => MapMaybe inr outr
+  ) =>
+  MapMaybe inr outr
 
-class MapMaybeRL
-      (inRl :: RowList)
-      (outRl :: RowList)
-      | inRl -> outRl
+class MapMaybeRL (inRl :: RowList) (outRl :: RowList) | inRl -> outRl
 
 instance mapMaybeRLNil :: MapMaybeRL Nil Nil
+
 instance mapMaybeRLCons :: MapMaybeRL tl tl' => MapMaybeRL (Cons k t tl) (Cons k (Maybe t) tl')
 
-foreign import fromORecordImpl :: forall required optional r. (forall a. Maybe a) -> (forall a. a -> Maybe a) -> Array String -> ORecord required optional -> { |r }
+foreign import fromORecordImpl :: forall required optional r. (forall a. Maybe a) -> (forall a. a -> Maybe a) -> Array String -> ORecord required optional -> { | r }
 
-allRequired :: forall required optional. RowKeys required => ORecord required optional -> { |required }
+allRequired :: forall required optional. RowKeys required => ORecord required optional -> { | required }
 allRequired = allRequiredImpl (rowKeys (RProxy :: _ required))
 
-foreign import allRequiredImpl :: forall r o. Array String -> ORecord r o -> { |r }
+foreign import allRequiredImpl :: forall r o. Array String -> ORecord r o -> { | r }
 
-allOptional :: forall required optional maybes. RowKeys optional => MapMaybe optional maybes => ORecord required optional -> { |maybes }
+allOptional :: forall required optional maybes. RowKeys optional => MapMaybe optional maybes => ORecord required optional -> { | maybes }
 allOptional = allOptionalImpl Nothing Just (rowKeys (RProxy :: _ optional))
 
-foreign import allOptionalImpl :: forall required optional r. (forall a. Maybe a) -> (forall a. a -> Maybe a) -> Array String -> ORecord required optional -> { |r }
+foreign import allOptionalImpl :: forall required optional r. (forall a. Maybe a) -> (forall a. a -> Maybe a) -> Array String -> ORecord required optional -> { | r }
 
 -- Accessors
-
 getRequired :: forall l a r' required optional. IsSymbol l => Row.Cons l a r' required => SProxy l -> ORecord required optional -> a
 getRequired sp r = getRequiredImpl (reflectSymbol sp) r
 
@@ -123,32 +120,34 @@ setRequired sp a r = setImpl (reflectSymbol sp) a r
 
 setOptional :: forall l a r' required optional. IsSymbol l => Row.Cons l a r' optional => SProxy l -> Maybe a -> ORecord required optional -> ORecord required optional
 setOptional sp (Just a) r = setImpl (reflectSymbol sp) a r
+
 setOptional sp Nothing r = deleteImpl (reflectSymbol sp) r
 
 foreign import setImpl :: forall a required optional. String -> a -> ORecord required optional -> ORecord required optional
+
 foreign import deleteImpl :: forall required optional. String -> ORecord required optional -> ORecord required optional
 
 -- Utils
-
-unrequire
-  :: forall required optional required' optional' unrequired
-     . Union optional unrequired optional'
-     => Union required' unrequired required
-     => ORecord required optional
-     -> ORecord required' optional'
+unrequire ::
+  forall required optional required' optional' unrequired.
+  Union optional unrequired optional' =>
+  Union required' unrequired required =>
+  ORecord required optional ->
+  ORecord required' optional'
 unrequire = unsafeCoerce
 
 -- Helpers
-
 rowKeys :: forall r. RowKeys r => RProxy r -> Array String
 rowKeys = List.toUnfoldable <<< _rowKeys
 
-class RowKeys (r :: # Type) where
+class RowKeys (r :: #Type) where
   _rowKeys :: RProxy r -> List String
 
-instance _rowKeysInstance :: ( RowToList r rl
-                            , RowKeysRL rl
-                            ) => RowKeys r where
+instance _rowKeysInstance ::
+  ( RowToList r rl
+  , RowKeysRL rl
+  ) =>
+  RowKeys r where
   _rowKeys _ = _rowKeysRL (RLProxy :: _ rl)
 
 class RowKeysRL (rl :: RowList) where
@@ -160,7 +159,8 @@ instance _rowKeysRLNil :: RowKeysRL Nil where
 instance _rowKeysRLCons ::
   ( IsSymbol name
   , RowKeysRL tl
-  ) => RowKeysRL (Cons name typ tl) where
+  ) =>
+  RowKeysRL (Cons name typ tl) where
   _rowKeysRL _ = List.Cons (reflectSymbol (SProxy :: _ name)) (_rowKeysRL (RLProxy :: _ tl))
 
 foreign import undefined :: forall a. a
--- test/Main.purs
+++ test/Main.purs
@@ -1,62 +1,64 @@
 module Test.Main
-       ( main
-       ) where
+  ( main
+  ) where
 
 import Prelude
-
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
 import Effect (Effect)
 import ORecord (ORecord, fromORecord, getOptional, getRequired, orecord, setOptional, setRequired, toORecord, unrequire)
 import Test.Assert (assert', assertFalse')
 
-type O = ORecord (i :: Int, s :: String) (b :: Boolean, n :: Number)
+type O
+  = ORecord ( i :: Int, s :: String ) ( b :: Boolean, n :: Number )
 
 sample :: O
 sample = orecord { i: 10, s: "foo", b: true }
 
 main :: Effect Unit
 main = do
-  assertFalse' "should not have key for undefined optional" $
-    hasKey "n" sample
-
-  assert' "store optionals directly" $
-    unsafeGet "b" sample == true
-
-  assert' "to orecord" $
-    sample == toORecord { i: 10
-                        , s: "foo"
-                        , b: Just true
-                        , n: Nothing
-                        }
-
-  assert' "from orecord" $
-    fromORecord sample == { i: 10
-                          , s: "foo"
-                          , b: Just true
-                          , n: Nothing
-                          }
-
-  assert' "gets required member" $
-    getRequired i_ sample == 10
-
-  assert' "gets defined optional member" $
-    getOptional b_ sample == Just true
-
-  assert' "gets undefined optional member" $
-    getOptional n_ sample == Nothing
-
-  assert' "get after set required" $
-    getRequired i_ (setRequired i_ 20 sample) == 20
-
-  assert' "get after set optional" $
-    getOptional b_ (setOptional b_ (Just false) sample) == (Just false)
-
-  assert' "get after unset optional" $
-    getOptional b_ (setOptional b_ Nothing sample) == Nothing
-
-  assertFalse' "unsetting removes key" $
-    sample # setOptional b_ Nothing # hasKey "b"
+  assertFalse' "should not have key for undefined optional"
+    $ hasKey "n" sample
+  assert' "store optionals directly"
+    $ unsafeGet "b" sample
+    == true
+  assert' "to orecord"
+    $ sample
+    == toORecord
+        { i: 10
+        , s: "foo"
+        , b: Just true
+        , n: Nothing
+        }
+  assert' "from orecord"
+    $ fromORecord sample
+    == { i: 10
+      , s: "foo"
+      , b: Just true
+      , n: Nothing
+      }
+  assert' "gets required member"
+    $ getRequired i_ sample
+    == 10
+  assert' "gets defined optional member"
+    $ getOptional b_ sample
+    == Just true
+  assert' "gets undefined optional member"
+    $ getOptional n_ sample
+    == Nothing
+  assert' "get after set required"
+    $ getRequired i_ (setRequired i_ 20 sample)
+    == 20
+  assert' "get after set optional"
+    $ getOptional b_ (setOptional b_ (Just false) sample)
+    == (Just false)
+  assert' "get after unset optional"
+    $ getOptional b_ (setOptional b_ Nothing sample)
+    == Nothing
+  assertFalse' "unsetting removes key"
+    $ sample
+    # setOptional b_ Nothing
+    # hasKey "b"
 
 i_ :: SProxy "i"
 i_ = SProxy
@@ -71,8 +73,7 @@ n_ :: SProxy "n"
 n_ = SProxy
 
 -- compile checks
-
-unrequiredSample :: ORecord (i :: Int) (s :: String, b :: Boolean, n :: Number)
+unrequiredSample :: ORecord ( i :: Int ) ( s :: String, b :: Boolean, n :: Number )
 unrequiredSample = unrequire sample
 
 foreign import hasKey :: forall optional required. String -> ORecord optional required -> Boolean
